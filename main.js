057862f5ed72d6d53cabb6a1043b6e7f7231e300
+ <+>
2022-08-26 09:49:04 +0300

    done assignment

--- main.js
+++ main.js
@@ -2,9 +2,34 @@
 const EMPTY_HEART = '♡'
 const FULL_HEART = '♥'
 
+
 // Your JavaScript code goes here!
+const likes = document.querySelectorAll('.like-glyph');
 
+let favorite = (e) => {
+  let emoji = e.target;
+  mimicServerCall()
+    .then(() => {
+      if (emoji.textContent === EMPTY_HEART) {
+        emoji.textContent = FULL_HEART;
+        emoji.className = "activated-heart";
+      }
+      else {
+        emoji.textContent = EMPTY_HEART;
+        emoji.className = "";
+    }
+    })
+    .catch((error) => {
+      const errorModal = document.getElementById('modal');
+      errorModal.className = "";
+      errorModal.textContent = error;
+      setTimeout(() => errorModal.className = "hidden", 3000);
+  })
+}
 
+for (let like of likes) {
+  like.addEventListener("click", favorite);
+}
 
 
 //------------------------------------------------------------------------------
